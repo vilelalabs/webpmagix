68a537bd1ea4db6f430720904eac87a736cfa281
vilelalabs <hvilelaeng@gmail.com>
2023-10-13 08:59:21 -0300

    fix: center text

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,7 +2,7 @@ const Title = () => {
     return ( 
         <div className="flex flex-col items-center gap-2">
           <h1 className="text-4xl text-titlered font-bold">WEBP Converter</h1>
-          <p className="text-2xl font-medium text-babypowder">Convert any Image to lightweight .webp format.</p>
+          <p className="text-2xl font-medium text-babypowder text-center">Convert any Image to lightweight .webp format.</p>
         </div>
      );
 }
