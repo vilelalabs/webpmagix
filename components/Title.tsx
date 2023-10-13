4eb4241431d6bb0f130a04a94f363da86ffc49ac
vilelalabs <hvilelaeng@gmail.com>
2023-10-13 03:28:28 -0300

    feat: create title component

---
+++ components/Title.tsx
@@ -0,0 +1,10 @@
+const Title = () => {
+    return ( 
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-4xl text-titlered font-bold">WEBP Converter</h1>
+          <p className="text-2xl font-medium text-babypowder">Convert any Image to lightweight .webp format.</p>
+        </div>
+     );
+}
+ 
+export default Title   ;
