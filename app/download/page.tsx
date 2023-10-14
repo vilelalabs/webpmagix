d62bf6bad4715a3d0927333cb1f9d40b0fd00b01
vilelalabs <hvilelaeng@gmail.com>
2023-10-14 13:46:47 -0300

    clear state on restarting with new files.

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -6,10 +6,20 @@ import Title from "@/components/Title";
 import ConvertedFile from "@/components/ConvertedFile";
 import useConvertedFiles, {ConvertedFileInterface} from "@/hooks/useConvertedFiles";
 import { useRouter } from "next/navigation";
+import useLoadedFiles from "@/hooks/useLoadedFiles";
 
 const Download = () => {
     const router = useRouter();
-    const { convertedFiles } = useConvertedFiles();
+    const { convertedFiles, clearConvertedFiles } = useConvertedFiles();
+    const { clearLoadedFiles } = useLoadedFiles();
+
+    const handleReturnToHome = () => {
+        clearConvertedFiles();
+        clearLoadedFiles();
+        router.replace("/");
+    }
+
+    if(convertedFiles.length === 0) router.replace("/");
 
     return ( 
         <div className="flex flex-col w-full items-center justify-center px-4 gap-24 md:px-52">
@@ -25,7 +35,7 @@ const Download = () => {
                 ))}
             </div>
             <button
-                onClick={() => router.push("/")}
+                onClick={handleReturnToHome}
                 className="text-redviolet bg-turquoise rounded-xl w-60 md:w-80 py-3 text-xl font-medium hover:bg-hoverturquoise m-4">Convert New Files</button>
         </div>
      );
