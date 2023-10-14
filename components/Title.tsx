b05233fcddda075c55f9bb61998f7927f0012fd4
vilelalabs <hvilelaeng@gmail.com>
2023-10-14 14:44:26 -0300

    fix: center title for mobile size

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,7 +1,7 @@
 const Title = () => {
     return ( 
         <div className="flex flex-col items-center gap-2 mt-16">
-          <h1 className="text-4xl text-titlered font-bold">WEBP Converter</h1>
+          <h1 className="text-4xl  text-center text-titlered font-bold">WEBP Converter</h1>
           <p className="text-2xl font-medium text-babypowder text-center">Convert any Image to lightweight .webp format.</p>
         </div>
      );
