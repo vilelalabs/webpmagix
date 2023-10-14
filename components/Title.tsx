74e9c199d352055ca33d4c6fe79177e2ca297c02
vilelalabs <hvilelaeng@gmail.com>
2023-10-14 09:29:19 -0300

    fix: adds margin top

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
     return ( 
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center gap-2 mt-16">
           <h1 className="text-4xl text-titlered font-bold">WEBP Converter</h1>
           <p className="text-2xl font-medium text-babypowder text-center">Convert any Image to lightweight .webp format.</p>
         </div>
