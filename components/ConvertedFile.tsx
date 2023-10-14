df853b71478a4b420bc013f83d68a8398c0603bc
vilelalabs <hvilelaeng@gmail.com>
2023-10-14 14:43:13 -0300

    fix: mobile responsive

--- components/ConvertedFile.tsx
+++ components/ConvertedFile.tsx
@@ -6,9 +6,9 @@ const ConvertedFiles = ({ name, size, downloadLink }: ConvertedFileInterface) =>
     const fileSize = size >= 1000000 ? `${(size / 1000000).toFixed(2)} MB` : `${(size / 1000).toFixed(2)} KB`
 
     return (
-        <div className="flex flex-row items-center justify-between border rounded-xl shadow-turquoise shadow-md  border-turquoise p-3">
-            <p className="text-richblack w-3/5">{name}</p>
-            <p className="text-redviolet w-1/5 text-right">{fileSize}</p>
+        <div className="flex flex-col lg:flex-row items-center justify-between border rounded-xl shadow-turquoise shadow-md  border-turquoise p-3 gap-3 lg:gap-0">
+            <p className="text-richblack lg:w-3/5">{name}</p>
+            <p className="text-redviolet lg:w-1/5 text-right">{fileSize}</p>
             <button className="text-babypowder bg-redviolet text-right rounded-2xl px-4 py-2 mr-[20px] ml-[30px] hover:bg-hoverredviolet">
                 <a className="flex flex-row items-center justify-between gap-2" href={downloadLink} download>
                     <p className="text-xl uppercase">Download</p>
--- components/LoadedFile.tsx
+++ components/LoadedFile.tsx
@@ -12,9 +12,9 @@ const LoadedFile = ({file}: LoadedFileInterface) => {
     const fileSize = file.size>=1000000 ? `${(file.size/1000000).toFixed(2)} MB` : `${(file.size/1000).toFixed(2)} KB`
 
     return (
-        <div className="flex flex-row items-center justify-between border rounded-xl shadow-turquoise shadow-md  border-turquoise p-3">
-            <p className="text-richblack w-3/5">{file.name}</p>
-            <p className="text-redviolet w-1/5 text-right">{fileSize}</p>
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-between border rounded-xl shadow-turquoise shadow-md  border-turquoise p-3 lg:gap-0 gap-2">
+            <p className="text-richblack text-center lg:text-left lg:w-3/5">{file.name}</p>
+            <p className="text-redviolet lg:w-1/5 text-right">{fileSize}</p>
             <AiOutlineClose
                 onClick={() => removeLoadedFile(file)}
                 size={25}
